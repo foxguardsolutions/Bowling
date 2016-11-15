39f04511b12f433cda8c847accad943163598a3e
Phillip Wright <pwright@foxguardsolutions.com>
2016-11-15 14:58:16 -0500

    Initial project version

---
+++ Bowling-kata/Bowling-kata.tests/TestClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling_kata.tests {
+    [TestFixture]
+    public class TestClass {
+        [Test]
+        public void testGeneric() {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void testRollValue() {
+            var strike = 'X';
+            var spare = '/';
+            var gutter = '-';
+            var other_roll = '5';
+
+            Assert.AreEqual(Bowling_kata.RollValue(strike), 10);
+            Assert.AreEqual(Bowling_kata.RollValue(spare), 10);
+            Assert.AreEqual(Bowling_kata.RollValue(gutter), 0);
+            Assert.AreEqual(Bowling_kata.RollValue(other_roll), 5);
+        }
+    }
+}
---
+++ Bowling-kata/Bowling-kata/BowlingKata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling_kata
+{
+    public class Bowling_kata {
+
+        static Dictionary<char, int> roll_values = new Dictionary<char, int>() {
+                {'X', 10},
+                {'/', 10},
+                {'-', 0},
+        };
+
+        public static int RollValue(char mark) {
+            if (roll_values.ContainsKey(mark)) {
+                return roll_values[mark];
+            } else {
+                return (int)Char.GetNumericValue(mark);
+            }
+        }
+    }
+}
