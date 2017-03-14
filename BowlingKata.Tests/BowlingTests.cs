b04bee4f89b197f2dc9baf23134536f8dc0acd01
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-03-14 15:45:49 -0400

    Bowling Task completed
    Ready for PR

---
+++ BowlingKata.Tests/BowlingTests.cs
@@ -0,0 +1,61 @@
+﻿using BowlingKata;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata.Tests
+{
+    [TestFixture]
+    class BowlingTests
+    {
+        Bowling game;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            game = new Bowling();
+        }
+
+        [Test]
+        public void TestGutterGame()
+        {
+            Assert.AreEqual(0, game.CalculateScore("--------------------"));
+        }
+
+        [TestCase("9", 0)] // Incomplete Frame
+        [TestCase("X9", 0)] // Incomplete Frame
+        [TestCase("XXX", 30)]
+        [TestCase("2/9", 19)]
+        [TestCase("--", 0)]
+        public void TestFrameScore(string rolls, int expectedScore)
+        {
+            Assert.AreEqual(expectedScore, game.CalculateScore(rolls));
+        }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            Assert.AreEqual(300, game.CalculateScore("XXXXXXXXXXXX"));
+        }
+
+        [TestCase("819-7263458163369-9-", 90)]
+        [TestCase("7---34--51-871528/9-", 71)]
+        [TestCase("-/-/9/X547-6-8-819/X", 127)]
+        [TestCase("XX9/XX7/XX2/XX5", 223)]
+        public void TestScore(string rolls, int expectedScore)
+        {
+            Assert.AreEqual(expectedScore, game.CalculateScore(rolls));
+        }
+
+        [TestCase("-/-/-/-/-/-/-/-/-/-/-", 100)]
+        [TestCase("-/1/2/3/4/5/6/7/8/9/5", 150)]
+        [TestCase("9/9/9/9/9/9/9/9/9/9/9", 190)]
+        public void TestSpare(string rolls, int expectedScore)
+        {
+            Assert.AreEqual(expectedScore, game.CalculateScore(rolls));
+        }
+    }
+}
---
+++ BowlingKata/Bowling.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    public class Bowling
+    {
+        private const char gutter = '-';
+        private const char spare = '/';
+        private const char strike = 'X';
+
+        private Dictionary<char, int> scoreValues = new Dictionary<char, int>()
+        {
+            { gutter, 0  },
+            { spare,  0  },
+            { strike, 10 }
+        };
+
+        public Bowling() { }
+
+        public int CalculateScore(string rolls)
+        {
+            int score = GetFrameScore(rolls);
+
+            if (GetFrameLength(rolls) <= rolls.Length)
+                score += CalculateScore(rolls.Substring(GetFrameLength(rolls)));
+
+            return score;
+        }
+
+        /*************** Private Functions ***************/
+        private int GetFrameLength(string rolls)
+        {
+            return HasStrike(rolls) ? 1 : 2;
+        }
+
+        private int GetFrameScore(string rolls)
+        {
+            int score = 0;
+
+            if (IsCompleteFrame(rolls))
+                score = HasMultiFrameScore(rolls) ? GetMultiFrameScore(rolls) : GetScoreOfTwoRolls(rolls);
+
+            return score;
+        }
+
+        private int GetMultiFrameScore(string rolls)
+        {
+            string nextFrame = rolls.Substring(GetFrameLength(rolls));
+            return HasStrike(rolls) ? GetStrikeScore(nextFrame) : GetSpareScore(nextFrame);
+        }
+
+        private int GetSpareScore(string rolls)
+        {
+            return 10 + GetScoreOfRoll(rolls[0]);
+        }
+
+        private int GetStrikeScore(string rolls)
+        {
+            return 10 + GetScoreOfTwoRolls(rolls);
+        }
+
+        private int GetScoreOfRoll(char roll)
+        {
+            return scoreValues.ContainsKey(roll) ? scoreValues[roll] : int.Parse(roll.ToString());
+        }
+
+        private int GetScoreOfTwoRolls(string rolls)
+        {
+            return HasSpare(rolls) ? 10 : GetScoreOfRoll(rolls[0]) + GetScoreOfRoll(rolls[1]);
+        }
+
+        private bool HasMultiFrameScore(string frame)
+        {
+            return HasSpare(frame) || HasStrike(frame);
+        }
+
+        private bool HasSpare(string frame)
+        {
+            return frame.Length > 2 && frame[1] == spare;
+        }
+
+        private bool HasStrike(string frame)
+        {
+            return frame.Length > 1 && frame[0] == strike;
+        }
+
+        private bool IsCompleteFrame(string frame)
+        {
+            return HasMultiFrameScore(frame) ?
+                   frame.Length >= 3 :
+                   frame.Length >= 2;
+        }
+    }
+}
