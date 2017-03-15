c9591b704f97879b62346724c17eac920d68f2b0
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-03-15 14:04:56 -0400

    More Unit Tests Written
    Unit Tests for FrameScoreCalculator have been completed.

--- BowlingKata.Tests/FrameScoreCalculatorTests.cs
+++ BowlingKata.Tests/FrameScoreCalculatorTests.cs
@@ -18,6 +18,60 @@ namespace BowlingKata.Tests
             _frameScoreCalculator = new FrameScoreCalculator();
         }
 
+        [Test]
+        public void GetFrameLength_GutterBalls_Length2()
+        {
+            Assert.AreEqual(2, _frameScoreCalculator.GetFrameLength("--"));
+        }
 
+        [TestCase("9-")]
+        [TestCase("815-")]
+        [TestCase("234561")]
+        public void GetFrameLength_OpenFrameVaryingRollLengths_Length2(string rolls)
+        {
+            Assert.AreEqual(2, _frameScoreCalculator.GetFrameLength(rolls));
+        }
+
+        [Test]
+        public void GetFrameLength_Spare_Length2()
+        {
+            Assert.AreEqual(2, _frameScoreCalculator.GetFrameLength("9/"));
+        }
+
+        [Test]
+        public void GetFrameLength_Stike_Length1()
+        {
+            Assert.AreEqual(1, _frameScoreCalculator.GetFrameLength("X"));
+        }
+
+        [TestCase("X-")]
+        [TestCase("8/")]
+        [TestCase("7")]
+        public void GetFrameScore_IncompleteScore_Score0(string rolls)
+        {
+            Assert.AreEqual(0, _frameScoreCalculator.GetFrameScore(rolls));
+        }
+
+        [TestCase("9-", 9)]
+        [TestCase("815-", 9)]
+        [TestCase("234561", 5)]
+        public void GetFrameScore_OpenFrameVaryingRollLengths_ScoreFirstTwoRolls(string frame, int expectedValue)
+        {
+            Assert.AreEqual(expectedValue, _frameScoreCalculator.GetFrameScore(frame));
+        }
+
+        [TestCase("9/-", 10)]
+        [TestCase("7/3", 13)]
+        [TestCase("5/9", 19)]
+        public void GetFrameScore_Spare_Score10PlusNexRoll(string frame, int expectedValue)
+        {
+            Assert.AreEqual(expectedValue, _frameScoreCalculator.GetFrameScore(frame));
+        }
+
+        [Test]
+        public void GetFrameScore_Turkey_Score30()
+        {
+            Assert.AreEqual(30, _frameScoreCalculator.GetFrameScore("XXX"));
+        }
     }
 }
